630f5a25eaf83fbdee7fba63271f0ad431540291
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-04-26 11:06:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cmath
+from collections import defaultdict
 
 class Qubit:
     def __init__(self, a, b):
@@ -12,6 +13,13 @@ class Qubit:
         probability_false = abs(self.a)**2
         probability_true = abs(self.b)**2
         return 'False' if probability_false > probability_true else 'True'
+    
+    def hadamard(self):
+        a = self.a
+        b = self.b
+        self.a = (a + b) / cmath.sqrt(2)
+        self.b = (a - b) / cmath.sqrt(2)
+
 
 class Basis:
     def __init__(self, basis):
@@ -20,9 +28,35 @@ class Basis:
     def __str__(self):
         return ', '.join(str(b) for b in self.basis)
 
+class QV:
+    def __init__(self, mapping):
+        self.mapping = defaultdict(complex, mapping)
+
+    def __str__(self):
+        return ', '.join(f'{k}: {v}' for k, v in self.mapping.items())
+
+def qv(pairs):
+    return QV(dict(pairs))
+
+# Exemplo de uso
+qFalse = QV({Qubit(1, 0): 1})
+qTrue = QV({Qubit(0, 1): 1})
+qFT = qv([(Qubit(1, 0), 1/cmath.sqrt(2)), (Qubit(0, 1), 1/cmath.sqrt(2))])
+
+print(qFalse)
+print(qTrue)
+print(qFT)
+
+
 qubit = Qubit(cmath.sqrt(0.5), cmath.sqrt(0.5))
 print(qubit)
 print("Measurement:", qubit.measure())
 
+qubit.hadamard()
+print(qubit)
+
 basis = Basis([Qubit(1, 0), Qubit(0, 1)])
 print(basis)
+
+mapping = qv([(Qubit(1, 0), 0.5), (Qubit(0, 1), 0.5)])
+print(mapping)
